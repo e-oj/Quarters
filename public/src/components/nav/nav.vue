77d0b4bf9f99afbb045cfe2735347c347b74e90e
emmanuel <eolaojo@yahoo.com>
2017-11-26 07:54:15 -0500

    routing + home and nav components

---
+++ public/src/components/nav/nav.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="nav">
+    <ul>
+      <li>About</li>
+      <li>Hosts</li>
+      <li>Rates</li>
+      <li>Team</li>
+      <li>Book Now</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style>
+  #nav{
+    position: relative;
+    left: 0;
+    top: 0;
+    height: 80px;
+    width: 100%;
+  }
+
+  #nav ul{
+    margin: 0;
+    list-style-type: none;
+    position: relative;
+  }
+
+  #nav li{
+    float: left;
+  }
+</style>
